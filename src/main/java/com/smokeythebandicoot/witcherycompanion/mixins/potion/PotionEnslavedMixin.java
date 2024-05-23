a9c8ff0e19d431f9dbf3cb79af3b43502ce7593e
Ashley Wright <ashley@msrandom.net>
2024-05-24 01:27:31 +0300

    Add fix for the Brew of Raising

---
+++ src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/potion/PotionEnslavedMixin.java
@@ -0,0 +1,25 @@
+package com.smokeythebandicoot.witcherycompanion.mixins.potion;
+
+import com.smokeythebandicoot.witcherycompanion.config.ModConfig;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.msrandom.witchery.potion.PotionEnslaved;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PotionEnslaved.class)
+public class PotionEnslavedMixin {
+	@SuppressWarnings("ConstantValue")
+	@Inject(method = "setEnslaverForMob", remap = false, cancellable = true, at = @At("HEAD"))
+	private static void setEnslaverForMob(EntityLiving entity, EntityPlayer player, CallbackInfoReturnable<Boolean> cir) {
+		if (!ModConfig.PatchesConfiguration.BrewsTweaks.raising_fixNullPlayerName) {
+			return;
+		}
+
+		if (player == null || player.getName() == null || player.getName().isEmpty()) {
+			cir.setReturnValue(false);
+		}
+	}
+}
