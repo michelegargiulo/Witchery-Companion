865ec6f9ae2e3f2d53ab537776052910b0f17d27
Michele Gargiulo <miki.gargiulo@live.it>
2024-08-02 12:10:12 +0200

    Add WitcheryBlockDoor to valid CursableTriggers

--- src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/block/WitcheryBlockDoorMixin.java
+++ src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/block/WitcheryBlockDoorMixin.java
@@ -1,32 +1,13 @@
 package com.smokeythebandicoot.witcherycompanion.mixins.block;
 
 import com.smokeythebandicoot.witcherycompanion.api.dispersaltrigger.ICursableTrigger;
-import com.smokeythebandicoot.witcherycompanion.config.ModConfig.PatchesConfiguration.BrewsTweaks;
-import net.minecraft.block.BlockContainer;
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyBool;
-import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.msrandom.witchery.block.WitcheryBlockButton;
 import net.msrandom.witchery.block.WitcheryBlockDoor;
-import net.msrandom.witchery.brewing.ModifiersImpact;
-import net.msrandom.witchery.brewing.action.BrewActionList;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
 
 /**
  Mixins:
@@ -39,28 +20,8 @@ public abstract class WitcheryBlockDoorMixin extends BlockDoor implements ICursa
         super(materialIn);
     }
 
-    /*
     @Override
-    public BlockPos getEffectivePos(World world, BlockPos pos) {
-        // When potion hits lower half
-        IBlockState state = world.getBlockState(pos);
-        if (state.getValue(BlockDoor.HALF) == BlockDoor.EnumDoorHalf.LOWER) {
-            return pos;
-        }
-
-        // For when the potion hits upper half
-        IBlockState lowerState = world.getBlockState(pos.down());
-        if (lowerState.getBlock() instanceof BlockDoor && lowerState.getValue(BlockDoor.HALF) == BlockDoor.EnumDoorHalf.LOWER) {
-            return pos.down();
-        }
-
-        // for weird edge cases when setBlockstate is used to place half-doors
-        return pos;
-    }
-     */
-
-    @Override
-    public boolean hasTileEntity(IBlockState state) {
+    public boolean hasTileEntity(@Nonnull IBlockState state) {
         return true;
     }
 }
