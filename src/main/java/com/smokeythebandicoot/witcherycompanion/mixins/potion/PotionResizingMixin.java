05d3dc68724e7c1525361abfedc890d9d19a351f
Michele Gargiulo <miki.gargiulo@live.it>
2024-08-06 09:26:32 +0200

    Tentative fix 1

--- src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/potion/PotionResizingMixin.java
+++ src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/potion/PotionResizingMixin.java
@@ -1,10 +1,18 @@
 package com.smokeythebandicoot.witcherycompanion.mixins.potion;
 
-import com.smokeythebandicoot.witcherycompanion.config.ModConfig;
+import com.smokeythebandicoot.witcherycompanion.config.ModConfig.PatchesConfiguration.PotionTweaks;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.msrandom.witchery.potion.PotionResizing;
+import net.msrandom.witchery.util.EntitySizeInfo;
+import net.msrandom.witchery.util.ResizingUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
@@ -13,28 +21,63 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
     blocks at the smallest size
  */
 @Mixin(PotionResizing.class)
-public class PotionResizingMixin {
+public abstract class PotionResizingMixin {
+
+    @Shadow(remap = false)
+    public static float getScaleFactor(int amplifier) { return 0; }
 
     @Inject(method = "getScaleFactor", remap = false, cancellable = true, at = @At("HEAD"))
     private static void modifyResizingRates(int amplifier, CallbackInfoReturnable<Float> cir) {
-        if (ModConfig.PatchesConfiguration.PotionTweaks.resizing_tweakCustomSizes) return;
+        if (PotionTweaks.resizing_tweakCustomSizes) return;
         float customValue = 1.0f;
         switch (amplifier) {
             case 0:
-                customValue = ModConfig.PatchesConfiguration.PotionTweaks.resizing_tweakCustomSizeSmallest;
+                customValue = PotionTweaks.resizing_tweakCustomSizeSmallest;
                 break;
             case 1:
-                customValue = ModConfig.PatchesConfiguration.PotionTweaks.resizing_tweakCustomSizeSmaller;
+                customValue = PotionTweaks.resizing_tweakCustomSizeSmaller;
                 break;
             case 2:
-                customValue = ModConfig.PatchesConfiguration.PotionTweaks.resizing_tweakCustomSizeBigger;
+                customValue = PotionTweaks.resizing_tweakCustomSizeBigger;
                 break;
             case 3:
-                customValue = ModConfig.PatchesConfiguration.PotionTweaks.resizing_tweakCustomSizeBiggest;
+                customValue = PotionTweaks.resizing_tweakCustomSizeBiggest;
                 break;
         }
         cir.setReturnValue(customValue);
 
     }
+
+    @Inject(method = "onLivingUpdate", remap = false, at = @At(value = "HEAD"))
+    private void accountCrouchHeightForResizing(World world, EntityLivingBase entity, LivingEvent.LivingUpdateEvent event, int amplifier, int duration, CallbackInfo ci) {
+        if (!PotionTweaks.resizing_fixEffectOnPlayers) {
+            return;
+        }
+
+        float reductionFactor = 0.03F * (float)(event.getEntity().world.isRemote ? 1 : 20);
+        if (world.isRemote || entity.ticksExisted % 20 == 0) {
+            EntitySizeInfo sizeInfo = new EntitySizeInfo(entity);
+            float scale = getScaleFactor(amplifier);
+            float requiredHeight = sizeInfo.defaultHeight * scale;
+            float requiredWidth = sizeInfo.defaultWidth * scale;
+            float currentHeight = event.getEntityLiving().height;
+            if (requiredHeight != currentHeight) {
+                if (entity instanceof EntityPlayer) {
+                    EntityPlayer player = (EntityPlayer)entity;
+                    if (!world.isRemote) {
+                        player.eyeHeight = currentHeight * 0.92F;
+                    }
+                }
+
+                entity.stepHeight = scale < 1.0F ? 0.0F : scale - 1.0F;
+                if (scale < 1.0F) {
+                    ResizingUtils.setSize(entity, Math.max(entity.width - reductionFactor, requiredWidth), Math.max(currentHeight - reductionFactor, requiredHeight));
+                } else {
+                    ResizingUtils.setSize(entity, Math.min(entity.width + reductionFactor, requiredWidth), Math.min(currentHeight + reductionFactor, requiredHeight));
+                }
+            }
+        }
+    }
+
 
 }
--- src/main/java/com/smokeythebandicoot/witcherycompanion/mixins_early/minecraft/entity/player/EntityPlayerMixin.java
+++ src/main/java/com/smokeythebandicoot/witcherycompanion/mixins_early/minecraft/entity/player/EntityPlayerMixin.java
@@ -3,12 +3,16 @@ package com.smokeythebandicoot.witcherycompanion.mixins_early.minecraft.entity.p
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smokeythebandicoot.witcherycompanion.config.ModConfig;
+import com.smokeythebandicoot.witcherycompanion.utils.Utils;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  Mixins:
@@ -23,19 +27,93 @@ public abstract class EntityPlayerMixin extends EntityLivingBase {
     @Unique
     public float witchery_Patcher$resizeScaleHeight = 1.0f;
 
+
     private EntityPlayerMixin(World worldIn) {
         super(worldIn);
     }
 
-    @WrapOperation(method = "updateSize", remap = true,
+    @Inject(method = "updateSize", remap = true, cancellable = true, at = @At(value = "HEAD"))
+    private void injectBeforeWitcheryTransformer(CallbackInfo ci) {
+        if (!ModConfig.PatchesConfiguration.PotionTweaks.resizing_fixEffectOnPlayers) {
+            return;
+        }
+
+        float width;
+        float height;
+
+        if (this.isElytraFlying()) {
+            width = 0.6F;
+            height = 0.6F;
+        } else if (this.isPlayerSleeping()) {
+            width = 0.2F;
+            height = 0.2F;
+        } else if (this.isSneaking()) {
+            width = 0.6F;
+            height = 1.65F;
+        } else {
+            width = 0.6F;
+            height = 1.8F;
+        }
+
+        width *= witchery_Patcher$resizeScaleWidth;
+        height *= witchery_Patcher$resizeScaleHeight;
+
+        if (width != this.width || height != this.height) {
+            AxisAlignedBB axisalignedbb = this.getEntityBoundingBox();
+            axisalignedbb = new AxisAlignedBB(axisalignedbb.minX, axisalignedbb.minY, axisalignedbb.minZ, axisalignedbb.minX + (double)width, axisalignedbb.minY + (double)height, axisalignedbb.minZ + (double)width);
+
+            if (!this.world.collidesWithAnyBlock(axisalignedbb)) {
+                this.setSize(width, height);
+            }
+        }
+
+        net.minecraftforge.fml.common.FMLCommonHandler.instance().onPlayerPostTick((EntityPlayer)(Object)this);
+        ci.cancel();
+    }
+
+
+    /** This Mixin makes it so that when returning player height the Brew of Resizing scale
+     * is taken into account. Note that before invoking Entity.setSize() player height is re-set
+     * to 1.8f or 1.65f if crouching, because for some reason this value is lost in EntityPlayer class
+     * when checking if (f != this.width || f1 != this.height) on line 405. */
+    /*@WrapOperation(method = "updateSize", remap = true,
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/EntityPlayer;setSize(FF)V", remap = true))
-    public void checkResizeWhenUpdatingSize(EntityPlayer instance, float width, float height, Operation<Void> original) {
+    private void checkResizeWhenUpdatingSize(EntityPlayer instance, float width, float height, Operation<Void> original) {
 
         if (ModConfig.PatchesConfiguration.PotionTweaks.resizing_fixEffectOnPlayers) {
-            original.call(instance, width * witchery_Patcher$resizeScaleWidth, height * witchery_Patcher$resizeScaleHeight);
+
+            //Utils.logChat(height * witchery_Patcher$resizeScaleHeight);
+            if (instance.isSneaking()) {
+                height = 1.65f;
+            } else {
+                height = 1.8f;
+            }
+            witchery_Patcher$effectiveWidth = width * witchery_Patcher$resizeScaleWidth;
+            witchery_Patcher$effectiveHeight = height * witchery_Patcher$resizeScaleHeight;
+            original.call(instance, witchery_Patcher$effectiveWidth, witchery_Patcher$effectiveHeight);
             return;
         }
         original.call(instance, width, height);
 
     }
+
+     */
+
+    /** This Mixin forces Vanilla to update player size even if there is no player-triggered size
+     * change. This fixes a bug that makes Resizing potion effect start shrinking or enlarging player
+     * only when the player sneaks or its height is changes in some other way */
+    /*@Inject(method = "updateSize", remap = true, at = @At(value = "INVOKE", remap = false, shift = At.Shift.BEFORE,
+            target = "Lnet/minecraftforge/fml/common/FMLCommonHandler;onPlayerPostTick(Lnet/minecraft/entity/player/EntityPlayer;)V"))
+    private void forceUpdateSize(CallbackInfo ci) {
+        if (witchery_Patcher$forceUpdateSize && witchery_Patcher$effectiveWidth > 0 && witchery_Patcher$effectiveHeight > 0 &&
+                ModConfig.PatchesConfiguration.PotionTweaks.resizing_fixEffectOnPlayers) {
+
+            Utils.logChat("Force update");
+            witchery_Patcher$forceUpdateSize = false;
+            setSize(witchery_Patcher$effectiveWidth, witchery_Patcher$effectiveHeight);
+            //this.eyeHeight = witchery_Patcher$effectiveHeight * 0.92;
+        }
+    }
+
+     */
 }
