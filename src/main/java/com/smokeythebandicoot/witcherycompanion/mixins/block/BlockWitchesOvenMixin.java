4bc2fb952ea2e25b62106125ab4f7fb07550c214
Michele Gargiulo <miki.gargiulo@live.it>
2024-10-04 12:43:25 +0200

    Fix particles spawning on Witches Oven

--- src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/block/BlockWitchesOvenMixin.java
+++ src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/block/BlockWitchesOvenMixin.java
@@ -18,12 +18,13 @@ import java.util.Random;
 @Mixin(BlockWitchesOven.class)
 public abstract class BlockWitchesOvenMixin {
 
+    /** This Mixin wraps the call to world.nextDouble() to have an offset of 0.25 multiplied by 16 / 6 **/
     @WrapOperation(method = "randomDisplayTick", remap = true, at = @At(
-            value = "INVOKE", target = "Ljava/util/Random;nextDouble()D"))
+            value = "INVOKE", target = "Ljava/util/Random;nextDouble()D", ordinal = 0))
     private double fixBurningParticlesHeight(Random instance, Operation<Double> original) {
         double result = original.call(instance);
         if (ModConfig.PatchesConfiguration.BlockTweaks.witchesOven_fixBurningParticlesHeight) {
-            return result + 0.25;
+            return result + 0.67;
         }
         return result;
     }
