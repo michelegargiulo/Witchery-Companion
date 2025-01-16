32dfaf5f860bad32ac432806fa3d5928c1e764b7
SmokeyTheBandicoot <miki.gargiulo@live.it>
2025-01-16 14:45:39 +0100

    Initial commit for crystal ball rework

---
+++ src/main/java/com/smokeythebandicoot/witcherycompanion/api/player/IPlayerExtendedDataAccessor.java
@@ -0,0 +1,9 @@
+package com.smokeythebandicoot.witcherycompanion.api.player;
+
+public interface IPlayerExtendedDataAccessor {
+
+    void setDivinationData(DivinationData data);
+
+    DivinationData getDivinationData();
+
+}
--- src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/witchery/extensions/PlayerExtendedDataMixin.java
+++ src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/witchery/extensions/PlayerExtendedDataMixin.java
@@ -1,12 +1,17 @@
 package com.smokeythebandicoot.witcherycompanion.mixins.witchery.extensions;
 
+import com.smokeythebandicoot.witcherycompanion.api.player.DivinationData;
+import com.smokeythebandicoot.witcherycompanion.api.player.IPlayerExtendedDataAccessor;
 import com.smokeythebandicoot.witcherycompanion.config.ModConfig;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameType;
 import net.msrandom.witchery.extensions.PlayerExtendedData;
 import net.msrandom.witchery.extensions.WitcheryExtendedData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,10 +22,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  * [Feature] Implement Throwing Skill
  */
 @Mixin(PlayerExtendedData.class)
-public abstract class PlayerExtendedDataMixin extends WitcheryExtendedData<EntityPlayer> {
+public abstract class PlayerExtendedDataMixin extends WitcheryExtendedData<EntityPlayer> implements IPlayerExtendedDataAccessor {
 
     @Shadow(remap = false)
     private int throwingSkill;
+
+    @Unique
+    private DivinationData witchery_Patcher$divinationData;
 
     /** This Mixin actually implements reading the Throwing Skill from player NBT **/
     @Inject(method = "read", remap = false, at = @At("HEAD"))
@@ -43,5 +51,27 @@ public abstract class PlayerExtendedDataMixin extends WitcheryExtendedData<Entit
         if (!ModConfig.PatchesConfiguration.BrewsTweaks.common_fixThrowingSkill) return;
         this.throwingSkill = Math.min(this.throwingSkill + 1, 100);
         cir.setReturnValue(throwingSkill);
+    }
+
+    /** This Mixin saves divination information to NBT data **/
+    @Inject(method = "write", remap = false, at = @At("HEAD"))
+    private void writeDivinationData(NBTTagCompound tag, CallbackInfo ci) {
+        DivinationData.writeToNBT(witchery_Patcher$divinationData, tag);
+    }
+
+    /** This Mixin reads divination information from NBT **/
+    @Inject(method = "read", remap = false, at = @At("HEAD"))
+    private void readDivinationData(NBTTagCompound tag, CallbackInfo ci) {
+        witchery_Patcher$divinationData = DivinationData.readFromNBT(tag);
+    }
+
+    @Override
+    public void setDivinationData(DivinationData data) {
+        this.witchery_Patcher$divinationData = data;
+    }
+
+    @Override
+    public DivinationData getDivinationData() {
+        return this.witchery_Patcher$divinationData;
     }
 }
