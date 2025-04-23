96fde06d72a4554167ee8e24ffb55a1c16876bfe
SmokeyTheBandicoot <miki.gargiulo@live.it>
2025-04-23 16:47:28 +0200

    Fix #89 Bark belt charges

--- src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/witchery/client/ClientEventsMixin.java
+++ src/main/java/com/smokeythebandicoot/witcherycompanion/mixins/witchery/client/ClientEventsMixin.java
@@ -19,12 +19,20 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
+/** Mixins:
+ * [Bugfix] Fix Bark Belt charges rendering transparency issues over armor bar
+ * **/
 @Mixin(ClientEvents.class)
 public abstract class ClientEventsMixin {
 
     @Shadow(remap = false) @Final
     private static ResourceLocation BARK_TEXTURES;
 
+    @Shadow(remap = false)
+    private static void drawTexturedModalRect(int par1, int par2, int par3, int par4, int par5, int par6) {
+    }
+
+    /** Save GL state, apply correct blending function and only then call drawTexturedModalRect. Then restore state **/
     @Inject(method = "drawBarkBeltCharges", remap = false, at = @At("HEAD"), cancellable = true)
     private static void fixBarkBeltChargesRendering(EntityPlayerSP player, int beltCharges, ScaledResolution screen, CallbackInfo ci) {
 
@@ -41,23 +49,25 @@ public abstract class ClientEventsMixin {
             int k2 = Math.max(10 - (j2 - 2), 3);
             int l2 = WitcheryResurrected.Companion.isTinkersPresent() ? i2 - 10 : i2 - (j2 - 1) * k2 - 10;
 
-            // Save GL State
+            // Save state
             GlStateManager.pushMatrix();
 
+            // Apply functions
             GlStateManager.enableBlend();
             GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
             GlStateManager.enableAlpha();
-            //GlStateManager.disableDepth();
 
             int iconOffsetX = 0;
             int ICON_WIDTH = 8;
             int ICON_HEIGHT = 8;
             int iconOffsetY = 248;
 
+            // Draw the rect
             for(int m = 0; m < beltCharges; ++m) {
-                mc.ingameGUI.drawTexturedModalRect(tx + m * ICON_HEIGHT, l2, iconOffsetX, iconOffsetY, ICON_WIDTH, ICON_HEIGHT);
+                drawTexturedModalRect(tx + m * ICON_HEIGHT, l2, iconOffsetX, iconOffsetY, ICON_WIDTH, ICON_HEIGHT);
             }
 
+            // Restore the state
             GlStateManager.popMatrix();
         }
 
